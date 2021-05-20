849f60d4a39e285a8e723e55495fc2d420361c2b
Julia Sulyaeva <jusu2968@hedenius.it.uu.se>
2021-05-20 23:08:25 +0200

    added fib_py() and tasks 4-5 (UNTESTED)

--- ou4_2/ou4_2.py
+++ ou4_2/ou4_2.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
 from heltal import Heltal
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
+
+def fib_py(n):
+	if n<= 1:
+		return n
+	else:
+		return(fib(n-1)+fib(n+1))
 
 def main():
-	f = Heltal(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	x = [n for n in range(30,46)]
+	y_py, y_c = [], []
+	for n in x:
+		start = pc()
+		fib_py(n)
+		end = pc()
+		y_py.append(end-start)
+	for n in x:
+		start = pc()
+		f = Heltal(n)
+		f.fib()
+		end = pc()
+		y_c.append(end-start)
+		plt.plot(x,y_py, color="blue", x, y_c, color="red")
+		plt.set(xlabel = "n", ylabel = "processing speed", title = "fibonacci speed for Python and C++")
+		plt.show()
+		plt.savefig("fib_speed.png")
+		f = Heltal(47)
+		print(f"47th fibonacci number: {f.fib()}")
 
 if __name__ == '__main__':
 	main()
